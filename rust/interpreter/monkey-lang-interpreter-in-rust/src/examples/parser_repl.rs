f5fc9e0d6790bfc2b843fea346f9b150c0466069
Yu Chen <yuchen@tsinghua.edu.cn>
2019-09-28 17:46:12 +0800

    add parser REPL in monkey-lang interpreter

---
+++ rust/interpreter/monkey-lang-interpreter-in-rust/src/examples/parser_repl.rs
@@ -0,0 +1,39 @@
+extern crate monkey;
+
+use std::io;
+use monkey::lexer::*;
+use monkey::parser::*;
+
+fn start<R: io::BufRead, W: io::Write>(mut reader: R, mut writer: W) -> io::Result<()> {
+
+    loop {
+        writer.write(b"> ");
+        writer.flush();
+        let mut line = String::new();
+        reader.read_line(&mut line)?;
+
+        let l = Lexer::new(&line);
+
+//        println!("======== Lexer Output ===========");
+//        for t in l {
+//            println!("{:?}",t);
+//        }
+
+        let mut p = Parser::new(l);
+        let prog = p.parse_program().unwrap();
+
+        println!("======== Parser Output ===========");
+        for s in prog.statements {
+            println!("{:?}",s);
+        }
+    }
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    println!("Welcome to the Monkey Parser REPL!");
+    let input = io::stdin();
+    let output = io::stdout();
+    let result = start(input.lock(), output.lock());
+    result
+}
