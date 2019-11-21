15f00cade3b215dbf7098c1045b698b29af2e6ad
Yu Chen <yuchen@tsinghua.edu.cn>
2019-11-21 10:41:39 +0800

    add pl0 compiler in java

---
+++ java/pl0/README.md
@@ -0,0 +1,1 @@
+pl0 compiler in java has been tested on openjdk 11.0.4 2019-07-16, ubuntu 19.10 x86-64
---
+++ java/pl0/src/Symbol.java
@@ -0,0 +1,10 @@
+/**
+ *¡¡¸÷ÖÖ·ûºÅµÄ±àÂë
+ */
+public enum Symbol {
+	nul, ident, number, plus, minus, times, slash,
+	oddsym, eql, neq, lss, leq, gtr, geq, lparen, rparen,
+	comma, semicolon, period, becomes, 
+	beginsym, endsym, ifsym, thensym, whilesym, writesym,
+	readsym, dosym, callsym, constsym, varsym, procsym
+}
